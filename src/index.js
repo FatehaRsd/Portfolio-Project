82b99fc77b1bbb666fa1b82023a898c2fabc97b9
FatehaRsd <nurulfateha.arsad@gmail.com>
2024-01-05 19:09:53 +0800

    add about page

---
+++ src/index.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+  $(".nav-link").on("click", function (e) {
+    if (this.hash !== "") {
+      e.preventDefault();
+
+      const hash = this.hash;
+
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        800,
+        function () {
+          window.location.hash = hash;
+        }
+      );
+    }
+  });
+});
+
+function scrollUp() {
+  $("html, body").animate(
+    {
+      scrollTop: 0,
+    },
+    800
+  );
+}
